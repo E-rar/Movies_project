4ecf485bf09fda84734baeb8c6332ba7899d0062
E-rar <erik.ramich@gmx.de>
2021-10-13 17:40:57 +0200

    pageRouting

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const axios = require('axios');
+var cors = require('cors')
+require('dotenv').config()
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
-var cors = require('cors')
+
+
 const port = process.env.PORT||3000
 app.use(cors())
 
@@ -11,15 +14,17 @@ app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
 })
 
-app.get('/news', (req, res) => {
-    axios.get(`https://api.themoviedb.org/3/movie/76341?api_key=${process.env.API_KEYY}`)
+app.get('/all/:page', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=${req.params.page}`)
         .then(function (response) {
             // handle success
-            console.log(response.data.articles);
-            res.render('news.ejs', { articles: response.data.articles })
+            console.log(response.data.results);
+            res.render('index.ejs', {movies: response.data.results,page:Number(req.params.page)})
+           
         })
         .catch(function (error) {
             // handle error
             console.log(error);
         })
 })
+
