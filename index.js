9a5e487dedc660b8d65e89699d66ee727c182541
E-rar <erik.ramich@gmx.de>
2021-10-14 16:05:11 +0200

    genres_search

--- index.js
+++ index.js
@@ -28,3 +28,33 @@ app.get('/all/:page', (req, res) => {
         })
 })
 
+app.get('/detail/:id', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.API_KEY}&language=en-US`)
+        .then(function (response) {
+            // handle success
+            console.log(response.data);
+            res.render('detail.ejs', {movie:response.data})
+           
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+})
+
+app.get(`/search/:genre/:page`, (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&page=${req.params.page}&with_genres=${req.params.genre}`)
+        .then(function (response) {
+            // handle success
+            console.log(response.data.results);
+            res.render('search.ejs', {movies: response.data.results,page:Number(req.params.page),genre:Number(req.params.genre)})
+           
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+})
+
+
+
