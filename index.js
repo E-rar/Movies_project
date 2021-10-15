e421710dd6a2d6b05107a8e68352fa9ca77ea466
E-rar <erik.ramich@gmx.de>
2021-10-15 12:45:58 +0200

    searchbar

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ var cors = require('cors')
 require('dotenv').config()
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 const port = process.env.PORT||3000
 app.use(cors())
 
-app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`)
-})
 
-app.get('/all/:page', (req, res) => {
+
+app.get('/all/:page', (req, res) => { //All Popular
     axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=${req.params.page}`)
         .then(function (response) {
             // handle success
@@ -28,7 +27,7 @@ app.get('/all/:page', (req, res) => {
         })
 })
 
-app.get('/detail/:id', (req, res) => {
+app.get('/detail/:id', (req, res) => { //Detail
     axios.get(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.API_KEY}&language=en-US`)
         .then(function (response) {
             // handle success
@@ -42,7 +41,7 @@ app.get('/detail/:id', (req, res) => {
         })
 })
 
-app.get(`/search/:genre/:page`, (req, res) => {
+app.get(`/search/:genre/:page`, (req, res) => { //Kategorie
     axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&page=${req.params.page}&with_genres=${req.params.genre}`)
         .then(function (response) {
             // handle success
@@ -56,5 +55,21 @@ app.get(`/search/:genre/:page`, (req, res) => {
         })
 })
 
+app.post('/ursearch', (req, res) => { //search-Funktion
+    res.redirect(`/ursearch/${req.body.search}/1`)
+})
 
+app.get('/ursearch/:search/:page', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${req.params.search}&page=${req.params.page}&include_adult=false`)
+        .then(function (response) {
+            res.render('ursearch.ejs', { search: req.params.search, movies: response.data.results, max: response.data.total_pages, page: req.params.page })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+})
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`)
+})
 
